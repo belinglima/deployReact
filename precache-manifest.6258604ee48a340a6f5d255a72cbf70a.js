dbcb3258fe46c94d87b7c9fb850bacc706d7176d
Salomão Beling <belinglima@gmail.com>
2019-11-09 17:53:47 -0300

    Updates

---
+++ precache-manifest.6258604ee48a340a6f5d255a72cbf70a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f28482152c9c7f61dddf1aa8881b92",
+    "url": "/deployReact/index.html"
+  },
+  {
+    "revision": "496bd6890f053b0a05f6",
+    "url": "/deployReact/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7ad6407afbafa618827c",
+    "url": "/deployReact/static/js/2.faaf063f.chunk.js"
+  },
+  {
+    "revision": "496bd6890f053b0a05f6",
+    "url": "/deployReact/static/js/main.19f948c5.chunk.js"
+  },
+  {
+    "revision": "95b4111645bd17342466",
+    "url": "/deployReact/static/js/runtime-main.eca9e576.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deployReact/static/media/logo.25bf045c.svg"
+  }
+]);
